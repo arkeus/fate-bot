9e3004f161aa8c9dcb5bca3b94acd60bb05db260
Lee Miller <iarkeus@gmail.com>
2013-08-17 04:09:23 -0700

    Start of a peek command.

---
+++ src/io/arkeus/fatebot/commands/impl/PeekCommand.java
@@ -0,0 +1,15 @@
+package io.arkeus.fatebot.commands.impl;
+
+import io.arkeus.fatebot.commands.Command;
+import io.arkeus.fatebot.commands.CommandException;
+
+public class PeekCommand extends Command {
+	public PeekCommand() {
+		super(0);
+	}
+
+	@Override
+	protected void run() throws CommandException {
+		return;
+	}
+}
