43256a44f6dde75ab489928dd1ce765f0b01aeab
Lee Miller <iarkeus@gmail.com>
2013-10-21 00:54:15 -0700

    Docs.

--- src/io/arkeus/fatebot/commands/InvalidCommandException.java
+++ src/io/arkeus/fatebot/commands/InvalidCommandException.java
@@ -1,5 +1,8 @@
 package io.arkeus.fatebot.commands;
 
+/**
+ * An exception used to indicate a command as not being valid.
+ */
 public class InvalidCommandException extends Exception {
 	private static final long serialVersionUID = -4656333502349209091L;
 
