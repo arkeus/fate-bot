5d226aa405f27a5c6491cb85a0747e61e2e6d2f5
Lee Miller <iarkeus@gmail.com>
2013-10-28 01:28:31 -0700

    Docs.

--- src/io/arkeus/fatebot/commands/impl/SayCommand.java
+++ src/io/arkeus/fatebot/commands/impl/SayCommand.java
@@ -3,9 +3,12 @@ package io.arkeus.fatebot.commands.impl;
 import io.arkeus.fatebot.commands.Command;
 import io.arkeus.fatebot.commands.CommandException;
 
+/**
+ * A command that causes the bot to relay a message to the channel.
+ */
 public class SayCommand extends Command {
 	public SayCommand() {
-		super(1);
+		super(1, true);
 	}
 
 	@Override
